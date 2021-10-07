3827ede603ec41579bc03488f76798c748523b22
Joltaz <jonatan.johansson.okomdal@gmail.com>
2021-10-07 14:31:21 +0200

    init

---
+++ base.js
@@ -0,0 +1,18 @@
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+    if (window.pageYOffset > 600) {//Show backToTopButton
+        backToTopButton.style.display = "block";
+    }
+    else {//Hide backToTopButton
+        backToTopButton.style.display = "none";
+    }
+}
+
+backToTopButton.addEventListener("click", backToTop);
+
+function backToTop() {
+    window.scrollTo(0, 0);
+}
